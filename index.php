476f2c39b656276cae00c46c9f7da9927d6c6f3d
Sasha <Sasha>
2021-09-10 15:30:03 +0700

    duck

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-php
+<?php
+interface FlyBehavior {
+	public function fly();
+}
+class FlyWithWings implements FlyBehavior {
+	public function fly() {
+		return 'Полет';
+	}
+}
+class FlyNoWay implements FlyBehavior {
+	public function fly() {
+		return 'Не летает';
+	}
+}
+class Duck {
+	public $flyBehavior;
+
+	public function performF1y() {
+		return $this->$flyBehavior->fly();
+	}
+}
+
+class MallardDuck extends Duck {
+	function ＿construct() {
+		this->$fly Behavior = new FlyWithWings();
+	}
+}
+class RubberDuck extends Duck {
+	function ＿construct() {
+		this->$fly Behavior = new FlyNollay();
+	}
+}
+
+function polet (Duck $duck) {
+	return sduck->performFly();
+}
+?>
+<html>
+<head></head>
+<body>
+<button onclick="alert('<?php echo polet(new MallardDuck())?>')">Кряква</button>
+<button onclick="alert('<?php echo polet(new RubberDuck())?>')">Резиновая уточка</button>
+</body>
+</html>
